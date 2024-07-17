e8cfecfefcde1ab2f8caf8654ee9b018f6c5c424
Zzq <1525761613@qq.com>
2024-07-17 17:42:00 +0800

    update:【修改Redis配置类】

--- src/main/java/com/work/workorganization/config/RedisConfig.java
+++ src/main/java/com/work/workorganization/config/RedisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import javax.annotation.Resource;
@@ -22,11 +23,14 @@ public class RedisConfig extends CachingConfigurerSupport {
     public RedisTemplate<Object, Object> redisTemplate() {
         //创建Redis模板对象
         RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+
         //默认的Key序列化器为：JdkSerializationRedisSerializer
         //StringRedisSerializer支持字符串类型的转化，而且默认使用UTF-8编码
         //下面代码的意思是使用StringRedisSerializer序列化器替换默认的Key序列化器JdkSerializationRedisSerializer
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
 
         redisTemplate.setConnectionFactory(connectionFactory);
         return redisTemplate;
