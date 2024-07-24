1caedbda6b520b1411ac8a09b795979eb0f453e7
Zzq <1525761613@qq.com>
2024-07-24 14:17:56 +0800

    update:【修改"异常处理器"】 1.支持不同类型对象处理其字段所对应的异常

--- src/main/java/com/work/workorganization/config/ExceptionControllerAdvice.java
+++ src/main/java/com/work/workorganization/config/ExceptionControllerAdvice.java
@@ -1,9 +1,9 @@
 package com.work.workorganization.config;
 
 import com.work.workorganization.pojo.ResponseBo;
-import com.work.workorganization.pojo.TestRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
@@ -24,27 +25,33 @@ import java.util.stream.Collectors;
 @Slf4j
 public class ExceptionControllerAdvice {
     @ExceptionHandler(RuntimeException.class)
-    public ResponseBo handlerNullException(RuntimeException runtimeException) {
-        return ResponseBo.error(runtimeException.getMessage());
+    public ResponseBo handlerNullException(RuntimeException e) {
+        return ResponseBo.error(e.getMessage());
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseBo handlerException(Exception e) {
+        return ResponseBo.error(e.getMessage());
     }
 
     /**
      * 参数校验异常处理
      */
     @ResponseStatus(HttpStatus.BAD_REQUEST) //设置状态码为 400
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseBo handlerMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    @ExceptionHandler({BindException.class,MethodArgumentNotValidException.class})
+    public ResponseBo handlerBindException(BindException e) {
         BindingResult exceptions = e.getBindingResult();
 
         if (exceptions.hasErrors()) {
 
-            List<FieldError> fieldErrors = exceptions.getFieldErrors();
-
+            List<FieldError> fieldErrors = exceptions.
+                    getFieldErrors();
+            // 获取当前触发异常的对象的类
+            Class<?> targetClass = Objects.requireNonNull(e.getTarget()).getClass();
             // 按照字段在类中的定义顺序排序
             fieldErrors = fieldErrors.stream()
                     .sorted((fe1, fe2) -> {
-                        int index1 = getFieldIndex(fe1.getField());
-                        int index2 = getFieldIndex(fe2.getField());
+                        int index1 = getFieldIndex(targetClass,fe1.getField());
+                        int index2 = getFieldIndex(targetClass,fe2.getField());
                         return Integer.compare(index1, index2);
                     })
                     .collect(Collectors.toList());
@@ -57,8 +64,8 @@ public class ExceptionControllerAdvice {
     }
 
     // 获取字段在类中的索引位置
-    private int getFieldIndex(String fieldName) {
-        Field[] fields = TestRequest.class.getDeclaredFields();
+    private int getFieldIndex(Class<?> clazz, String fieldName) {
+        Field[] fields = clazz.getDeclaredFields();
         for (int i = 0; i < fields.length; i++) {
             if (fields[i].getName().equals(fieldName)) {
                 return i;
